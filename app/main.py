cd1fdd75d41cc7a249e42d1822ecaffc7199676a
Bryan Lee <bryanleee134@gmail.com>
2024-07-03 10:17:04 +0800

    run migrations on startup. use uvicorn instead of gunicorn

--- app/main.py
+++ app/main.py
@@ -1,12 +1,28 @@
+import logging
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from alembic.config import Config
+from alembic import command
 from app.database import engine, Base
 
+log = logging.getLogger("uvicorn.error")
+
 Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-
 # app.mount("/static", StaticFiles(directory="static"), name="static") # put back when it's ready
+
+def run_migrations():
+    log.info("Beginning migrations")
+    alembic_cfg = Config("alembic.ini")
+    command.upgrade(alembic_cfg, "head")
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    log.info("Starting up")
+    run_migrations()
+    yield
+    log.info("Shutting down")
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def read_root():
