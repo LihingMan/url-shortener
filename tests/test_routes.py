57b4b58ab4de1c7c81323cc2a19d16b8a74d3b80
Bryan Lee <bryanleee134@gmail.com>
2024-07-04 17:08:24 +0800

    add salt per session and use to generate shorturl

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -58,18 +58,61 @@ def test_read_form(client):
     assert "text/html" in response.headers['content-type']
 
 def test_shorten_url(client, mock_db_session, mock_get_title_tag_from_url):
+    salt = "testsalt"
+    url = "http://example.com"
+
     with pytest.MonkeyPatch.context() as m:
         m.setattr("app.helpers.get_title_tag_from_url", mock_get_title_tag_from_url)
         mock_db_session.query.return_value.filter.return_value.first.return_value = None
 
-        url = "http://example.com"
-        response = client.post("/shorten", data={"url": url})
+        response = client.post("/shorten", data={"url": url, "salt": salt})
         assert response.status_code == 200
         assert 'short_url' in response.json()
         assert 'title' in response.json()
         assert 'target_url' in response.json()
         assert 'short_url_hash' in response.json()
         mock_db_session.add.assert_called()
+
+def test_shorten_url_with_same_salt(client, mock_db_session, mock_get_title_tag_from_url):
+    salt = "testsalt"
+    url = "http://example.com"
+
+    with pytest.MonkeyPatch.context() as m:
+        m.setattr("app.helpers.get_title_tag_from_url", mock_get_title_tag_from_url)
+        mock_db_session.query.return_value.filter.return_value.first.return_value = None
+
+        response1 = client.post("/shorten", data={"url": url, "salt": salt})
+        response1_obj = response1.json()
+        assert response1.status_code == 200
+
+        response2 = client.post("/shorten", data={"url": url, "salt": salt})
+        response2_obj = response2.json()
+        assert response2.status_code == 200
+
+        assert response1_obj["short_url_hash"] == response2_obj["short_url_hash"]
+        assert response1_obj["short_url"] == response2_obj["short_url"]
+        assert response1_obj["target_url"] == response2_obj["target_url"]
+
+def test_shorten_url_with_different_salts(client, mock_db_session, mock_get_title_tag_from_url):
+    salt1 = "testsalt1"
+    salt2 = "testsalt2"
+    url = "http://example.com"
+
+    with pytest.MonkeyPatch.context() as m:
+        m.setattr("app.helpers.get_title_tag_from_url", mock_get_title_tag_from_url)
+        mock_db_session.query.return_value.filter.return_value.first.return_value = None
+
+        response1 = client.post("/shorten", data={"url": url, "salt": salt1})
+        response1_obj = response1.json()
+        assert response1.status_code == 200
+
+        response2 = client.post("/shorten", data={"url": url, "salt": salt2})
+        response2_obj = response2.json()
+        assert response2.status_code == 200
+
+        assert response1_obj["short_url_hash"] != response2_obj["short_url_hash"]
+        assert response1_obj["short_url"] != response2_obj["short_url"]
+        assert response1_obj["target_url"] == response2_obj["target_url"]
 
 def test_redirect_to_url(client, mock_db_session, mock_get_geo_from_ip):
     url = "http://example.com"
